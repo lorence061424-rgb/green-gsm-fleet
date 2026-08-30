12b37455d1c5c60303f936c08cf8cbb1f983f7bd
Green GSM Admin <greengsm@fleet.com>
2026-08-30 23:23:47 +0800

    Regenerate enhanced high-precision Burndown Chart Draw.io diagram for Chapter 3 Figure 2

--- scratch/regenerate_burndown_chart.py
+++ scratch/regenerate_burndown_chart.py
@@ -1,4 +1,11 @@
-<mxfile host="Electron" modified="2026-08-30T23:23:00.000Z" agent="Mozilla/5.0" version="21.6.8" type="device">
+import os
+
+downloads_dir = r"c:\xamppp\htdocs\TNVS\public\downloads"
+user_downloads = os.path.expanduser(r"~\Downloads")
+os.makedirs(downloads_dir, exist_ok=True)
+
+# High-precision Draw.io XML for Figure 2: Burndown Chart
+fig2_detailed_xml = """<mxfile host="Electron" modified="2026-08-30T23:23:00.000Z" agent="Mozilla/5.0" version="21.6.8" type="device">
   <diagram id="Figure2_Detailed" name="Figure 2 - Sprint Burndown Chart">
     <mxGraphModel dx="1200" dy="800" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="1169" pageHeight="827" math="0" shadow="0">
       <root>
@@ -99,4 +106,13 @@
       </root>
     </mxGraphModel>
   </diagram>
-</mxfile>
+</mxfile>"""
+
+# Save to public/downloads and User's Downloads
+pub_path = os.path.join(downloads_dir, "Figure2_Burndown_Chart.drawio")
+usr_path = os.path.join(user_downloads, "Figure2_Burndown_Chart.drawio")
+
+with open(pub_path, "w", encoding="utf-8") as f: f.write(fig2_detailed_xml)
+with open(usr_path, "w", encoding="utf-8") as f: f.write(fig2_detailed_xml)
+
+print("Successfully regenerated Figure2_Burndown_Chart.drawio!")
